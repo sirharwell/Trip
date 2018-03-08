0a153bc8c00556a76be11e5fca43979dcdc3458a
Ashley Margallo <amarie921@yahoo.com>
2018-03-08 16:05:03 -0700

    app.js crud

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,46 @@ class App extends Component {
   state = { trips: [] }
 
   componentDidMount() {
+    fetch('/api/items')
+      .then( res => res.json() )
+      .then( trips => this.setState({ trips }) )
   }
 
   addTrip = (name) => {
+    let trip = { name };
+    fetch('/api/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(trip)
+    }).then( res => res.json() )
+      .then( trip => {
+        const { trips } = this.state;
+        this.setState({ trips: [...trips, trip] });
+    })
   }
 
   updateTrip = (id) => {
+    fetch('/api/trips/${id}', { method: 'PUT'})
+      .then( res => res.json() )
+      .then( trip => {
+        let trips = this.state.trips.map( t => {
+          if (t.id === id)
+            return item
+          return t;
+      })
+     this.setState({ trips });
+    }) 
   }
 
   deleteTrip = (id) => {
+    fetch('/api/trips/${id}', { method: 'DELETE'})
+      .then( () => {
+        const { trips } = this.state;
+        this.setState({ trips: trips.filter( t => t.id !== id ) })
+      })
   }
 
   render() {
