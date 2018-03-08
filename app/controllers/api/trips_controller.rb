0a4ec02954d6161be6acfdedc9190fbc2bda90af
Bobby Caya <bob.caya1@gmail.com>
2018-03-08 14:41:32 -0700

    Add controller for Trip

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,2 +1,31 @@
 class Api::TripsController < ApplicationController
-end
+  def index 
+    render json: Trip.all 
+  end 
+
+  def create 
+    trip = Trip.new(trip_params)
+    if trip
+      render json: trip 
+    else 
+      render json: { errors: trip.errors}, status: unprocessable_entity
+    end
+  end 
+
+  def update 
+    trip = Trip.find(params[:id])
+    trip.update(complete: !trip.complete)
+    render json: trip 
+  end  
+
+  def destroy 
+    Trip.find(params[:id])
+    render json: {message: 'Trip Delete'}
+  end
+
+  private 
+
+  def trip_params 
+    params.require(:trip).permit(:name, :complete)
+  end 
+end 
