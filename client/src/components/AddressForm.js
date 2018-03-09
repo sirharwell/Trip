d2750b6b3ccd23a9ae06603d414e35d65e1f039c
Ashley Margallo <amarie921@yahoo.com>
2018-03-09 14:25:03 -0700

    addressform.js

---
+++ client/src/components/AddressForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class AddressForm extends React.Component {
+  state = { street: '', city: '', state: '', zip: '' }
+
+  handleChange = (e) => {
+    this.setState({ 
+      street: e.target.value,
+      city: e.target.value,
+      state: e.target.value,
+      zip: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addAddress(this.state.name);
+    this.setState({ street: '', city: '', state: '', zip: '' })
+  }
+
+
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="Add An Address"
+          required
+            value={this.state.street}
+            value={this.state.city}
+            value={this.state.state}
+            value={this.state.zip}
+          onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
+}
+
+export default TripForm;
