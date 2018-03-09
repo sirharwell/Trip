0be654a39b6a61b39fa7540372d8f3e0cad1f1ea
Ian Harwell <sirharwell@gmail.com>
2018-03-09 15:27:39 -0700

    minor

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -11,7 +11,7 @@ const styles = {
 const Trip = ({id, complete, name, updateTrip, deleteTrip, showStops, setShowing }) => (
   <div className="col s8">
     <div className="col m8">
-      <div style={ styles.pointer } onClick={() => setShowing(id)}>{name}</div>
+      <div style={ styles.pointer } onClick={() => setShowing(id)}>{id}) {name}</div>
     </div>
     <div style={ styles.pointer } className="col m1" onClick={() => deleteTrip(id)}>
       X
